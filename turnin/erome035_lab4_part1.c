affc2b708ca4d867cd415e688883b0042b971237
EmilyRomero19 <76976095+EmilyRomero19@users.noreply.github.com>
2021-04-17 14:32:05 -0700

    Update erome035_lab4_part1.c

--- turnin/erome035_lab4_part1.c
+++ turnin/erome035_lab4_part1.c
@@ -12,7 +12,7 @@
 #include "simAVRHeader.h"
 #endif
 
-enum SM1_STATES { SM1_SMStart, SM1_LED1, /*SM1_RELEASED1,*/ SM1_LED2, SM1_RELEASED2 } SM1_STATE;
+enum SM1_STATES { SM1_SMStart, SM1_LED1, SM1_RELEASED1, SM1_LED2, SM1_RELEASED2 } SM1_STATE;
 void Tick_LED() {
 	switch(SM1_STATE){
 
@@ -22,21 +22,21 @@ void Tick_LED() {
 
 	case SM1_LED1:
 	if( (PINA & 0x01) == 0x01){ //if PAO pressed
-	SM1_STATE = SM1_LED2;
+	SM1_STATE = SM1_RELEASED1;
 	}
 	else{
 	SM1_STATE = SM1_LED1;
 	}
 	break;
 	
-	/*case SM1_RELEASED1:
+	case SM1_RELEASED1:
 	if( (PINA & 0x01) == 0x01){ //if PAO pressed
 	SM1_STATE = SM1_LED2;
 	}
 	else{
 	SM1_STATE = SM1_RELEASED1;
 	}
-	break;*/		
+	break;		
 					
 	case SM1_LED2:
 	if( (PINA & 0x01) == 0x01){
@@ -58,6 +58,7 @@ void Tick_LED() {
 	
 	default:
 	SM1_STATE = SM1_LED1;
+	PORTB = 0x01;
 	break;
 }
 	switch(SM1_STATE){
@@ -69,8 +70,8 @@ void Tick_LED() {
 	PORTB = 0x01;
 	break;
 	
-	//case SM1_RELEASED1:
-	//break;
+	case SM1_RELEASED1:
+	break;
 			
 	case SM1_LED2:
 	PORTB = 0x02;
